cb89365b9790efc857fb10033ad6143fc46a937f
eehuerta@ucsd.edu <eehuerta@ucsd.edu>
2019-07-09 17:04:40 -0700

    basic random dice app finished need to fix orientation bugs

--- DiceRollerApp/DiceRollerApp/ViewController.swift
+++ DiceRollerApp/DiceRollerApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
     
@@ -17,16 +19,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    // this gets called when the screen loads
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateDiceImages()
     }
-
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
     // this is the controller for the roll button
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    // function that will get a random image and save it to the view
+    func updateDiceImages() {
         // this will create random numbers from 0 and 5
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        // gets different images depending on the numbers generated
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
